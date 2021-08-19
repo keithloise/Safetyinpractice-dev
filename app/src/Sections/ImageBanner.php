989d1d3d1947ff6cf43c329aa6c1ca4a12592ef4
DESKTOP-UJBPHJG\Keith <k8t_loise16@yahoo.com>
2021-08-19 15:50:40 +1200

    update image banner

--- app/src/Sections/ImageBanner.php
+++ app/src/Sections/ImageBanner.php
@@ -2,7 +2,11 @@
 
 namespace {
 
+    use SilverStripe\AssetAdmin\Forms\UploadField;
     use SilverStripe\Assets\Image;
+    use SilverStripe\Forms\DropdownField;
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 
     class ImageBanner extends Section
     {
@@ -27,5 +31,34 @@ namespace {
         private static $defaults = [
             'ImageHeight' => 'bh-large'
         ];
+
+        public function getSectionCMSFields(FieldList $fields)
+        {
+            $fields->addFieldToTab('Root.Main', $image = UploadField::create('Image', 'Banner image'));
+            $image->setFolderName('Sections/Section_Banner/Images');
+            $image->setAllowedExtensions(['png','gif','jpeg','jpg']);
+            $fields->addFieldToTab('Root.Main', DropdownField::create('ImageHeight', 'Image height',
+                array(
+                    'bh-small' => 'Small',
+                    'bh-medium'=> 'Medium',
+                    'bh-large' => 'Large'
+                )
+            ));
+            $fields->addFieldToTab('Root.Main', HTMLEditorField::create('Content'));
+            $fields->addFieldToTab('Root.Main', DropdownField::create('ContentPosition', 'Content position',
+                array(
+                    'cp-left'   => 'Left',
+                    'cp-center' => 'Center',
+                    'cp-right'  => 'Right'
+                )
+            ));
+            $fields->addFieldToTab('Root.Main', DropdownField::create('ImageOverlay', 'Add image overlay',
+                array(
+                    'none' => 'None',
+                    'overlay-dark' => 'Dark',
+                    'overlay-light'=> 'Light'
+                )
+            ));
+        }
     }
 }
