a5987e5862793aa613fd301debbe8f4e64b61a9e
DESKTOP-UJBPHJG\Keith <k8t_loise16@yahoo.com>
2021-08-19 15:47:19 +1200

    update image banner

--- app/src/Sections/ImageBanner.php
+++ app/src/Sections/ImageBanner.php
@@ -5,5 +5,13 @@ namespace {
     class ImageBanner extends Section
     {
         private static $singular_name = 'Image banner';
+
+        private static $db = [
+            'Content'         => 'HTMLText',
+            'ContentPosition' => 'Varchar',
+            'ImageAnimation'  => 'Varchar',
+            'ImageOverlay'    => 'Varchar',
+            'ImageHeight'     => 'Varchar',
+        ];
     }
 }
