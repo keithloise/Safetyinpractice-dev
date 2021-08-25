5f5ddaf46eec68f91e0eafdc384d889ca37cac50
Keith L <k8t_loise16@yahoo.com>
2021-08-25 21:15:34 +1200

    update Section.php

--- app/src/Sections/Section.php
+++ app/src/Sections/Section.php
@@ -2,16 +2,23 @@
 
 namespace {
 
+    use SilverStripe\AssetAdmin\Forms\UploadField;
+    use SilverStripe\Assets\Image;
     use SilverStripe\Core\ClassInfo;
     use SilverStripe\Forms\CheckboxField;
     use SilverStripe\Forms\DropdownField;
     use SilverStripe\Forms\FieldList;
     use SilverStripe\Forms\HiddenField;
     use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+    use SilverStripe\Forms\ListboxField;
     use SilverStripe\Forms\Tab;
     use SilverStripe\Forms\TabSet;
     use SilverStripe\Forms\TextField;
+    use SilverStripe\ORM\ArrayList;
     use SilverStripe\ORM\DataObject;
+    use SilverStripe\View\ArrayData;
+    use TractorCow\Colorpicker\Color;
+    use TractorCow\Colorpicker\Forms\ColorField;
 
     class Section extends DataObject
     {
@@ -24,12 +31,21 @@ namespace {
             'SectionType' => 'Varchar',
             'SectionWidth'=> 'Varchar',
             'SectionContainer' => 'Varchar',
+            'SectionPadding'   => 'Varchar',
+            'SectionBgType'    => 'Varchar',
+            'SectionBgColor'   => Color::class,
+            'SectionSecBgColor'=> Color::class,
             'Archived' => 'Boolean',
             'Sort'     => 'Int'
         ];
 
         private static $has_one = [
             'Page' => Page::class,
+            'SectionBgImage' => Image::class
+        ];
+
+        private static $owns = [
+            'SectionBgImage'
         ];
 
         private static $summary_fields = [
@@ -83,6 +99,25 @@ namespace {
                 )
             )->setDescription('<b>Fix-width</b>, which sets a max-width at each responsive breakpoint</br><b>Container fluid</b>, which is width: 100% at all breakpoints.</br>'));
 
+            $fields->addFieldToTab('Root.Settings', ListboxField::create('SectionPadding', 'Section Paddings',
+                Paddings::get()->map('Class', 'Name')));
+
+            $fields->addFieldToTab('Root.Settings', DropdownField::create('SectionBgType', 'Section background type',
+                array(
+                    'none'             => 'None',
+                    'bg-image' => 'Background image',
+                    'bg-color' => 'Background color',
+                    'bg-gradient' => 'Background gradient'
+                )
+            ));
+
+            $fields->addFieldToTab('Root.Settings', $bgImage = UploadField::create('SectionBgImage', 'Section background image'));
+                $bgImage->displayIf('SectionBgType')->isEqualTo('background-image')->orIf('SectionBgType')->isEqualTo('bac');
+                $bgImage->setFolderName('Sections/Background');
+
+            $fields->addFieldToTab('Root.Settings', $bgColor = ColorField::create('SectionBgColor', 'Section background color'));
+                $bgColor->displayIf('SectionBgType')->isEqualTo('background-color');
+
             $fields->addFieldToTab('Root.Main', CheckboxField::create('Archived'));
             $fields->addFieldToTab('Root.Main', HiddenField::create('Sort'));
 
@@ -116,6 +151,20 @@ namespace {
         public function getDisplayTypeTrim()
         {
             return str_replace(' ','', self::singleton($this->SectionType)->singular_name());
+        }
+
+        public function getReadablePaddings()
+        {
+            $output = new ArrayList();
+            $paddings = json_decode($this->SectionPadding);
+            if ($paddings) {
+                foreach ($paddings as $padding) {
+                    $output->push(
+                        new ArrayData(array('Name' => $padding))
+                    );
+                }
+            }
+            return $output;
         }
 
         public function getStatus()
